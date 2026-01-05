dc5bdab802715ffb835bd8e77684672a3a764177
Erick Alcantar <erick.alcantar@hotmail.com>
2026-01-04 19:16:49 -0600

    Mejorando workflow y script

--- hello-world.py
+++ hello-world.py
@@ -1,7 +1,8 @@
-#!/usr/bin/env python3
+import os
 
 def main():
-    print("hello world")
+    nombre = os.getenv("USERNAME")
+    print("hello world, this is {nombre} from GitHub Actions")
 
 if __name__ == "__main__":
     main()
