e7ac7f9a49843f7fb5e21520d82ab2ca51d041ee
Erick Alcantar <erick.alcantar@hotmail.com>
2026-01-04 19:21:34 -0600

    Bugfix

--- hello-world.py
+++ hello-world.py
@@ -2,7 +2,7 @@ import os
 
 def main():
     nombre = os.getenv("USERNAME")
-    print("hello world, this is {nombre} from GitHub Actions")
+    print(f"hello world, this is {nombre} from GitHub Actions")
 
 if __name__ == "__main__":
     main()
